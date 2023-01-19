62e0d24f8313f765c7e71b61c41ec913d811ff8d
Dmitry <nik.dima001@yandex.ru>
2023-01-19 16:15:52 +0300

    added dir game

---
+++ brain_games/games/brain_even.py
@@ -0,0 +1,18 @@
+from random import randint
+
+
+START = 1
+END = 999
+TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def is_even(num):
+    return num % 2 == 0
+
+
+def question_and_answer():
+    question = randint(START, END)
+    correct_answer = 'no'
+    if is_even(question):
+        correct_answer = 'yes'
+    return question, 
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.engine import run
+import brain_games.games.brain_even
+
+
+def main():
+    run(brain_games.games.brain_even)
+
+
+if __name__ == '__main__':
+    main()
